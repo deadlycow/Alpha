077b33e491b7f0be34d4bc83b6636bacc9ab1551
deadlycow-andreas <pew.andreas.k@gmail.com>
2025-03-15 13:59:15 +0100

    added some popout menus

--- Pressentation_MVC/wwwroot/js/site.js
+++ Pressentation_MVC/wwwroot/js/site.js
@@ -11,6 +11,32 @@
     }
 }
 
+const toggleShow = (e) => {
+    let targetClass = "";
+    let containerClass = "";
+
+    if (e.currentTarget.classList.contains("profile-container")) {
+        targetClass = ".options-container";
+    }
+    if (e.currentTarget.classList.contains("project-more")) {
+        const parent = e.currentTarget.closest(".project-card");
+        if (!parent) return;
+        targetClass = ".edit-container";
+        containerClass = parent;
+    }
+
+    const div = containerClass ? containerClass.querySelector(targetClass) : document.querySelector(targetClass);
+    if (div) {
+        div.classList.toggle("show");
+    }
+};
+
 document.querySelectorAll(".toggle-password").forEach(icon => {
     icon.addEventListener("click", togglePassword);
 });
+
+document.querySelectorAll(".project-more").forEach(more => {
+    more.addEventListener("click", toggleShow);
+});
+document.querySelector(".profile-container").addEventListener("click", toggleShow);
+
