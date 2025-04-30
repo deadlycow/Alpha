be50a55352c83d677a7b8b35dbfd7c57eb72c0d8
deadlycow-andreas <pew.andreas.k@gmail.com>
2025-04-30 15:01:56 +0200

    we now have cookieconsent working for this site

---
+++ Pressentation_MVC/Controllers/CookieConsentController.cs
@@ -0,0 +1,65 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pressentation_MVC.Controllers
+{
+  public class CookieConsentController : Controller
+  {
+    [HttpPost]
+    public IActionResult Setcookies([FromBody] CookieConsent consent)
+    {
+      if (consent == null)
+        return BadRequest();
+
+      Response.Cookies.Append("SessionCookie", "Essential", new CookieOptions
+      {
+        IsEssential = true,
+        Expires = DateTimeOffset.UtcNow.AddYears(1)
+      });
+
+      if (consent.Functional)
+      {
+        Response.Cookies.Append("FunctionalCookie", "Non-Essential", new CookieOptions
+        {
+          IsEssential = false,
+          Expires = DateTimeOffset.UtcNow.AddDays(90),
+          SameSite = SameSiteMode.Lax,
+          Path = "/"
+        });
+      }
+      else
+      {
+        Response.Cookies.Delete("FunctionalCookie");
+      }
+      if (consent.Analytics)
+      {
+        Response.Cookies.Append("AnalyticsCookie", "Non-Essential", new CookieOptions
+        {
+          IsEssential = false,
+          Expires = DateTimeOffset.UtcNow.AddDays(90),
+          SameSite = SameSiteMode.Lax,
+          Path = "/"
+        });
+      }
+      else
+      {
+        Response.Cookies.Delete("AnalyticsCookie");
+      }
+      if (consent.Marketing)
+      {
+        Response.Cookies.Append("MarketingCookie", "Non-Essential", new CookieOptions
+        {
+          IsEssential = false,
+          Expires = DateTimeOffset.UtcNow.AddDays(90),
+          SameSite = SameSiteMode.Lax,
+          Path = "/"
+        });
+      }
+      else
+      {
+        Response.Cookies.Delete("MarketingCookie");
+      }
+      return Ok();
+    }
+  }
+}
